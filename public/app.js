0ea25a0a813e9f648e8978b1581768ca927712c8
vernistage <sdickers10@gmail.com>
2016-12-26 20:00:54 -0600

    editable timer list component

--- public/app.js
+++ public/app.js
@@ -15,3 +15,29 @@ const TimersDashboard = React.createClass({
     );
   },
 });
+
+// immediate child compononent
+const EditableTimerList = React.createClass({
+  render: function () {
+    return (
+      <div id='timers'>
+        // immediate child
+        // passing down props to each child
+        <EditableTimer
+          title='Learn React'
+          project='Web Domination'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimerList
+          title='Learn extreme ironing'
+          project='World Domination'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    );
+  },
+});
