71c95e6061f922578279d2e355ce039b083ba625
vernistage <sdickers10@gmail.com>
2016-12-26 19:55:17 -0600

    parent component

--- public/app.js
+++ public/app.js
@@ -1,1 +1,17 @@
-
+// parent compononent
+const TimersDashboard = React.createClass({
+  render: function () {
+    return (
+      <div className='ui three column centered grid'>
+        <div className='column'>
+          // immediate children
+          <EditableTimerList />
+          // TimersDashboard passing one prop down: isOpen
+          <ToggleableTimerForm
+            isOpen={true}
+          />
+        </div>
+      </div>
+    );
+  },
+});
